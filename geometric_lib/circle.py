fbbf9bc2eebe70a6c40c3777229c04608a528d26
Khasanova Tatyana <230450524+ssobaka2021-max@users.noreply.github.com>
2025-10-14 09:32:40 +0300

    Update circle.py

--- geometric_lib/circle.py
+++ geometric_lib/circle.py
@@ -1,20 +1,21 @@
 import math
 
-
 def area(r):
     '''
-    Принимает число r - радиус круга, возвращает  произведение числа пи на квадрат r - площадь круга
+    Принимает число r - радиус круга, возвращает произведение числа пи на квадрат r - площадь круга
     '''
     if r < 0:
-        raise ValueError()
+        raise ValueError("Radius cannot be negative")
+    if r == 0:
+        return 0
     return math.pi * r * r
-
 
 def perimeter(r):
     '''
     Принимает число r - радиус круга, возвращает произведение числа пи на 2 и на r - периметр круга
     '''
     if r < 0:
-        raise ValueError()
+        raise ValueError("Radius cannot be negative")
+    if r == 0:
+        return 0
     return 2 * math.pi * r
-
