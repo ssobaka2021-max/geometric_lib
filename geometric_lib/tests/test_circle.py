b07250b1c22b3009fb3570680ad09a0d0f4527a2
Khasanova Tatyana <230450524+ssobaka2021-max@users.noreply.github.com>
2025-10-14 09:32:14 +0300

    Update test_circle.py

--- geometric_lib/tests/test_circle.py
+++ geometric_lib/tests/test_circle.py
@@ -4,25 +4,23 @@ from circle import *
 
 class CircleTestCase(unittest.TestCase):
     def test_circle_area_normal(self):
-        self.assertAlmostEqual(area(5), math.pi * 25)
-        self.assertAlmostEqual(area(2.5), math.pi * 6.25)
+        self.assertAlmostEqual(area(5), math.pi * 25, places=7)
+        self.assertAlmostEqual(area(2.5), math.pi * 6.25, places=7)
 
     def test_circle_area_zero(self):
-        self.assertEqual(area(0),0)
+        self.assertEqual(area(0), 0)
 
     def test_circle_area_negative(self):
         with self.assertRaises(ValueError):
             area(-5)
 
     def test_circle_perimeter_normal(self):
-        self.assertAlmostEqual(perimeter(10), 2 * math.pi * 10)
-        self.assertAlmostEqual(perimeter(4.5), 2 * math.pi * 4.5)
+        self.assertAlmostEqual(perimeter(10), 2 * math.pi * 10, places=7)
+        self.assertAlmostEqual(perimeter(4.5), 2 * math.pi * 4.5, places=7)
 
     def test_circle_perimeter_zero(self):
-        self.assertEqual(perimeter(0),0)
+        self.assertEqual(perimeter(0), 0)
 
     def test_circle_perimeter_negative(self):
         with self.assertRaises(ValueError):
             perimeter(-5)
-
-
